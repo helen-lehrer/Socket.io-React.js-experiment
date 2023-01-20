3d3696a3c407aa639836248849366807e511cdd4
Helen Lehrer <hlehrer@outlook.com>
2023-01-19 16:14:11 -0800

    add useEffect to receive socket data

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
 import io from 'socket.io-client';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
   const sendMessage = ()  => {
-    socket.emit("send_message", {message: "hello"});
+    socket.emit("send_message", { message });
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message);
+      setMessageReceived(data.message);
     })
-  }, [socket])
+  }, [socket]);
 
   return (
     <div className="App">
-      <input placeholder="Message..." />
+      <input placeholder="Message..." onChange={(event) => {
+        setMessage(event.target.value);
+      }}/>
         <button onClick={sendMessage}>Send Message</button>
+        <h1>Message:</h1>
+        {messageReceived}
     </div>
   );
 }
